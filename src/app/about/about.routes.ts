334465c916227a23a62252dcb0e1602ceda62aea
IMMIGRATION <johnnygerald516@gmail.com>
2024-07-12 23:50:17 +0300

    Creasting home services nav bar and about component

---
+++ src/app/about/about.routes.ts
@@ -0,0 +1,13 @@
+
+import { Routes } from "@angular/router";
+import { AboutComponent } from "./about.component";
+
+
+export default [
+{
+    path:"",
+    component:AboutComponent
+}
+
+
+] as Routes
---
+++ src/app/services/services.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from "@angular/router";
+import { ServicesComponent } from "./services.component";
+
+
+export default [
+{
+    path:"",
+    component:ServicesComponent
+}
+
+
+] as Routes
